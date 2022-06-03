d47a34286f45c80bc535cfd47714aaeefc2c3370
Ajay Patel <ajpatel762@gmail.com>
2022-06-04 00:05:02 +0530

    fixed internal server error

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import './index.css'
 import store from './store'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://ecommerce-drf.herokuapp.com'
-axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = 'https://ecommerce-drf.herokuapp.com'
+// axios.defaults.baseURL = "http://localhost:8000"
 
 const app = createApp(App).use(store)
 
